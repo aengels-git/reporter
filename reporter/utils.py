bd302db82da59deeb7d5cb59cf274cff3578bcb1
u890640 <alexander.engels@dak.de>
2023-10-27 11:00:28 +0200

    added prep_format

--- reporter/utils.py
+++ reporter/utils.py
@@ -5,6 +5,36 @@ import pickle
 import string
 import matplotlib.pyplot as plt 
 
+from typing import Union
+import pandas as pd
+def prep_format(
+        tab:pd.DataFrame,
+        int_cols: Union[None,str,list] = None,
+        percent_cols: Union[None,str,list] = None,
+        float_cols: Union[None,str,list] = None,
+        digits = 2):
+    if isinstance(percent_cols,str):
+        percent_cols = [percent_cols]
+    if isinstance(float_cols,str):
+        float_cols = [float_cols]
+    if all([col is None for col in [int_cols,percent_cols,float_cols]]):
+        # default all numeric:
+        format_mapping = {col: f"{{:,.{digits}f}}" for col in tab.select_dtypes(include='number').columns}
+        for key, value in format_mapping.items():
+            tab[key] = tab[key].apply(value.format)
+    else:
+        if int_cols is not None:
+            tab[int_cols] = tab[int_cols].astype(int)
+        if percent_cols is not None:
+            format_mapping = {col: f"{{:,.{digits}%}}" for col in percent_cols}
+            for key, value in format_mapping.items():
+                tab[key] = tab[key].apply(value.format)
+        if float_cols is not None:
+            format_mapping = {col: f"{{:,.{digits}f}}" for col in float_cols}
+            for key, value in format_mapping.items():
+                tab[key] = tab[key].apply(value.format)
+        # integers, percents und floats separat angeben 
+    return tab
 class DataPlot:
     def __init__(self,plot, width=10, height=6):
         storage_path = path.join(environ['APPDATA'], "reporter")
